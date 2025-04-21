7ca212c510032d909e43cd704ebe590d45ee62ee
22624 <22624@1k129s10.dune.edu>
2025-04-21 10:34:16 +0200

    изменение1

--- src/ustal2/Ustal2.java
+++ src/ustal2/Ustal2.java
@@ -17,6 +17,9 @@ public class Ustal2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("задание1");
+        /**
+         * изменение1
+         */
     }
     
 }
